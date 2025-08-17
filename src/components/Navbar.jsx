697f01ab5b9f101ffd750d9a6018d1e698fbe0d6
MihirSharma0 <154828607+MihirSharma0@users.noreply.github.com>
2025-08-18 00:41:03 +0530

    commit-2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
             <img src={logo} alt="logo" className="w-14 h-16 object-cover" />
           
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Mustaqeem &nbsp;
+            Mihir &nbsp;
             <span className="sm:block hidden"> | Devxora</span>
           </p>
         </Link>
